b75ef0e015929c61ac4e33148d603eabe8129c78
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2020-12-18 02:54:34 +0530

    added navbar partial

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,6 @@
 var functions = firebase.functions()
-var initialTimeValue = 8
-var currentTimerValue = 8
+var initialTimeValue = 500
+var currentTimerValue = 500
 let responsesBody = {
   Management: [],
   Design: [],
