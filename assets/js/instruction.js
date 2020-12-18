a3c7108a33bc178b8948e0ce6767ab78827a55e4
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2020-12-19 00:52:34 +0530

    added check if test was already given

--- assets/js/instruction.js
+++ assets/js/instruction.js
@@ -1,9 +1,14 @@
 auth.onAuthStateChanged(async (user) => {
   if (user) {
     //now we check if user is already in db
+    console.log(user.uid)
     const doc = await db.collection('Users').doc(user.uid).get()
     if (!doc.exists) {
       window.location.href = './index.html'
+    }
+    if ((await db.collection('Attempted').doc(user.uid).get()).exists) {
+      alert('Test already attempted!')
+      auth.signOut()
     }
   } else {
     // User is signed out
@@ -11,5 +16,3 @@ auth.onAuthStateChanged(async (user) => {
     window.location.href = './index.html'
   }
 })
-
-window.addEventListener('DOMContentLoaded', () => {})
